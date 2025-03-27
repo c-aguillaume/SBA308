052c204bda4f053a914fea7a0b4ebb4b32b58652
c-aguillaume <175580634+c-aguillaume@users.noreply.github.com>
2025-03-27 09:19:46 -0400

    update to code

--- SBA308.js
+++ SBA308.js
@@ -1,22 +1,6 @@
-/*
-Objectives:
-1. Employ basic JavaScript syntax accurately.
-2. Implement control flow structures such as conditionals and loops effectively.
-3. Use arrays and objects to organize and manage data.
-4. Develop functions to create reusable code.
-5. Utilize loops and iteration to navigate through data collections.
-6. Implement error handling to manage potential code failures gracefully.
-*/
 
-/*Create a script that:
-1. Gathers Data
-2. Processes it
-3. Outputs a consistent result as described by a specification.
-*/ 
 
 //Create a function named getLearnerData() that accepts these values as parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission]), and returns the formatted result, which should be an array of objects as described above.
-
-
 
 
 const CourseInfo = {
@@ -97,44 +81,206 @@ const CourseInfo = {
   ];
 
 
+ 
 
+let learnerObject1 = {       //template object for each of the students
+    id: null,
+    avg: null,
+    1: null,
+    2: null,
+  }
 
-let gradeOneResult = function (score, points_possible){ //function to get the result for Grade One
-  return score / points_possible
+  let learnerObject2 = {       //template object for each of the students
+    id: null,
+    avg: null,
+    1: null,
+    2: null,
+  }
+
+ 
+  // Check if the assignment group belongs to the course.
+function isValidCourseAssignmentGroup(CourseInfo, AssignmentGroup) {
+  return CourseInfo.id === AssignmentGroup.course_id;
 }
 
-let gradeTwoResult = function (score, points_possible) { //function to get the result for Grand Two
-  return score / points_possible
-}
+// Check if a learner's submission is valid.
+function isValidSubmission(submission, assignment) {
+  const score = submission.submission.score;
+  const pointsPossible = assignment.points_possible;
 
-let classScoreAverage = function (gradeOneResult, gradeTwoResult) {
-  return gradeOneResult / gradeTwoResult
-}
-
-//Pull Learner submissions make a copy of it and turn them into an array. Use spread operator
-let LearnerSubmissionCopy = [...LearnerSubmissions];
-
-//Pull all Learner ID 125 Assignments into an array
-let learnerOneAssignments = []
-
-//Use loop iteration to find every assignment Learner 125 turned in.
-for(let a = 0; a < LearnerSubmissionCopy.length; a++){
-  if(LearnerSubmissionCopy[a].learner_id === 125){
-    learnerOneAssignments.push(LearnerSubmissionCopy[a]);
+  if (pointsPossible === 0 || typeof score !== "number" || isNaN(score)) {
+    return false;
+  } else { 
+    return true;
   }
 }
 
-  console.log(learnerOneAssignments)
-
-
-
-
-let learnerObject = {       //template object for each of the students
-  id: null,
-  avg: null,
-  1: null,
-  2: null,
+// Calculate the weighted average of a learner's scores.
+function calculateWeightedAverage(learnerData) {
+  return (learnerData.totalScore / learnerData.totalWeight) * 100;
 }
+
+// Process learner data, calculate scores, and return the results.
+function processLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
+  if (!isValidCourseAssignmentGroup(CourseInfo, AssignmentGroup)) {
+    throw new Error("Invalid input: AssignmentGroup does not belong to the course.");
+  }
+
+  const assignments = AssignmentGroup.assignments;
+  const assignmentScores = {};
+  const learnerData = {};
+
+  for (const submission of LearnerSubmissions) {
+    const learnerID = submission.learner_id;
+    const assignmentID = submission.assignment_id;
+    const assignment = assignments.find((a) => a.id === assignmentID);
+
+    if (!assignment || new Date(submission.submission.submitted_at) > new Date(assignment.due_at)) {
+      continue;
+    }
+
+    if (isValidSubmission(submission, assignment)) {
+      if (!learnerData[learnerID]) {
+        learnerData[learnerID] = {
+          id: learnerID,
+          totalScore: 0,
+          totalWeight: 0,
+        };
+      }
+
+      const score = submission.submission.score;
+      const pointsPossible = assignment.points_possible;
+      learnerData[learnerID].totalScore += (score / pointsPossible) * pointsPossible;
+      learnerData[learnerID].totalWeight += pointsPossible;
+      assignmentScores[assignmentID] = (score / pointsPossible) * 100;
+    }
+  }
+
+  return { learnerData, assignmentScores };
+}
+
+// Get and format learner data, including scores and averages.
+function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
+  try {
+    const { learnerData, assignmentScores } = processLearnerData(
+      CourseInfo,
+      AssignmentGroup,
+      LearnerSubmissions
+    );
+  
+    const results = [];
+  
+    for (const learnerID in learnerData) {
+      const learner = learnerData[learnerID];
+      const weightedAverage = calculateWeightedAverage(learner);
+  
+      const learnerResult = {
+        id: learner.id,
+        avg: weightedAverage,
+      };
+  
+      for (const assignmentID in assignmentScores) {
+        learnerResult[assignmentID] = assignmentScores[assignmentID];
+      }
+  
+      results.push(learnerResult);
+    }
+  
+    return results;
+    
+  } catch(error) {
+    console.error(error.message);
+  }
+ 
+}
+
+// Get learner data and handle potential errors.
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /// Trying different things out 
+//  LearnerSubmissions.forEach((x) => {
+//   let idNum = x.learner_id
+//   let assignOne = x.submission.score
+//   if(idNum === 125){
+//     learnerObject1["id"] = idNum;
+//     learnerObject1["1"] = assignOne;
+//   }
+//   if(idNum === 125 && x.submission)
+//   let assignTwo = x.submission.score
+//  })
+//  console.log(learnerObject1)
+
+
+
+
+
+
+
+
+
+ // for(loop through learner submissions){
+//   if (id is equal to 125) {
+//     return the value 125 in learnerObject1
+//     else if (assignment id is equal to 1){
+//       return the value of assignment 1 in learnerObject1 assignment 1
+//     }
+//   } else {
+
+//   }
+// }
+
+
+// let getAssignmentsbyLearnerID = function(learnerId){
+// return learnerSubmissionCopy.filter(function(submission) {
+//   return submission.learner_id === learnerId
+// })
+// }
+// let learnerOneAssignments = getAssignmentsbyLearnerID(125)
+// console.log(learnerOneAssignments)
+
+// //Pull all Learner ID 125 Assignments into an array
+// // let learnerOneAssignments = []
+
+// //Use loop iteration to find every assignment Learner 125 turned in.
+// for(let a = 0; a < learnerSubmissionCopy.length; a++){
+//   if(learnerSubmissionCopy[a].learner_id === 125){
+//     learnerOneAssignments.push(learnerSubmissionCopy[a]);
+//   } 
+// }
+// //Did Learner 125 submit assignment one? True/False
+// let Submitted = learnerOneAssignments[0].submission.score ? "True" : "False"
+//   // console.log(Submitted)
+
+// //If yes return score. If no return null
+
+// //Find if you submitted Assignment One.True/False. If true return the score 
+// let learnerOneScores = []
+// for(let a = 0; a <learnerOneAssignments ) {
+// }
+
+// console.log(learnerOneAssignments)
+// console.log(learnerOneAssignments[0].submission.score)
+
+  // console.log(learnerOneAssignments)
+
+
+
+
+
 
 
 /*
@@ -144,7 +290,7 @@ console.log(finalGrade)
 
 */
 
-let finalGrade = LearnerSubmissions[0].submission.score //thhis is how you pull specific data from an array
+// let finalGrade = LearnerSubmissions[0].submission.score //thhis is how you pull specific data from an array
 // console.log(finalGrade)
 
   // Get the key value pair "ID"  and "ID number" from Learner Submissions. What is needed to do so?
@@ -200,3 +346,16 @@ let finalGrade = LearnerSubmissions[0].submission.score //thhis is how you pull 
 //         console.log(greeting(firstName[oneName], lastName[oneName]))
 //     }
 
+//Functions I might use
+
+// let gradeOneResult = function (score, points_possible){ //function to get the result for Grade One
+//   return score / points_possible
+// }
+
+// let gradeTwoResult = function (score, points_possible) { //function to get the result for Grand Two
+//   return score / points_possible
+// }
+
+// let classScoreAverage = function (gradeOneResult, gradeTwoResult) {
+//   return gradeOneResult / gradeTwoResult
+// }
